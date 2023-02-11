04efc94a268d772c07e486e9bd187acca7de42e1
pipikai20 <2064548872@qq.com>
2023-02-11 16:56:06 +0800

    搜索历史，清空问题需要更正

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,11 +6,8 @@
       <div class="nav-container">
         <!-- 主导航栏上半部分 - 页面跳转 -->
         <el-row type="flex" class="nav-bar">
-          <img
-            src="@/static/assets/logo-text.svg"
-            class="logo-text"
-            style="width: 107px;cursor: pointer;" @click="jumpToIndex()"
-          />
+          <img src="@/static/assets/logo-text.svg" class="logo-text" style="width: 107px;cursor: pointer;"
+            @click="jumpToIndex()" />
           <img src="@/static/assets/logo.svg" alt class="logo-img" />
           <!-- 主导航栏左半部分，包含链接 -->
           <el-col :span="15" class="left">
@@ -21,9 +18,7 @@
                 <i class="el-icon-caret-bottom"></i>
               </span>
               <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item
-                  ><span @click="jumpToIndex()">首页</span></el-dropdown-item
-                >
+                <el-dropdown-item><span @click="jumpToIndex()">首页</span></el-dropdown-item>
                 <el-dropdown-item>沸点</el-dropdown-item>
                 <el-dropdown-item>直播</el-dropdown-item>
                 <el-dropdown-item>活动</el-dropdown-item>
@@ -34,11 +29,7 @@
             </el-dropdown>
             <!-- 完整的导航链接 -->
             <el-menu default-active="0" class="nav-link" mode="horizontal">
-              <el-menu-item
-                v-for="(item, index) in titleList"
-                :key="index"
-                :index="index.toString()"
-              >
+              <el-menu-item v-for="(item, index) in titleList" :key="index" :index="index.toString()">
                 <el-link :href="item.url" target="_blank">
                   {{ item.title }}
                   {{ index }}
@@ -47,11 +38,7 @@
               </el-menu-item>
               <el-submenu v-show="moreShow" index="8">
                 <template slot="title">更多</template>
-                <el-menu-item
-                  v-for="(item, index) in moreList"
-                  :key="index"
-                  :index="`8-${index.toString()}}`"
-                >
+                <el-menu-item v-for="(item, index) in moreList" :key="index" :index="`8-${index.toString()}}`">
                   <el-link :href="item.url" target="_blank">
                     {{ item.title }}
                     {{ index }}
@@ -65,62 +52,31 @@
           <el-col :span="14" class="right">
             <!-- 输入框 -->
             <div>
-              <el-input
-                ref="input"
-                v-model="input"
-                placeholder="探索稀土掘金"
-                class="input"
-                @focus="inPut()"
-                @blur="outPut()"
-                @keydown.enter.native="outPut()"
-              >
+              <el-input ref="input" v-model="input" placeholder="探索稀土掘金" class="input" @focus="inPut()" @blur="outPut()"
+                @keydown.enter.native="outPut()">
               </el-input>
               <transition name="el-zoom-in-left">
-                <div
-                  v-show="!badgeShow"
-                  class="searchMenu"
-                  :style="{ width: searchWidth }"
-                >
+                <div v-show="!badgeShow" class="searchMenu" :style="{ width: searchWidth }">
                   <div class="searchHead">
                     <span>搜索历史</span>
                     <span class="clear"> 清空</span>
                   </div>
                   <div class="searchList">{{}}</div>
-                  </div>
-                  </transition>
+                </div>
+              </transition>
               <div class="search" :class="searchChange">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
-                  fill="none"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                   <path
                     d="M12.4008 12.4008C14.744 10.0577 14.744 6.25871 12.4008 3.91556C10.0577 1.57242 6.25871 1.57242 3.91556 3.91556C1.57242 6.25871 1.57242 10.0577 3.91556 12.4008C6.25871 14.744 10.0577 14.744 12.4008 12.4008ZM12.4008 12.4008L15.5828 15.5828"
-                    :stroke="searchColor"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
+                    :stroke="searchColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
               </div>
             </div>
             <!-- 创作者中心按钮 -->
             <div class="holder">
               <transition name="badge">
-                <el-badge
-                  v-show="badgeShow"
-                  :value="1"
-                  class="badge"
-                  type="danger"
-                >
-                  <el-dropdown
-                    split-button
-                    type="primary"
-                    size="small"
-                    class="originator-drop"
-                  >
+                <el-badge v-show="badgeShow" :value="1" class="badge" type="danger">
+                  <el-dropdown split-button type="primary" size="small" class="originator-drop">
                     创作者中心
                     <el-dropdown-menu slot="dropdown">
                       <div @click="jump2Editor()">
@@ -140,18 +96,10 @@
               </transition>
             </div>
             <!-- vip 图标 -->
-            <img
-              src="@/static/assets/logo-vip.svg"
-              class="vip"
-            />
+            <img src="@/static/assets/logo-vip.svg" class="vip" />
 
             <!-- 消息图标 -->
-            <el-button
-              type="text"
-              size="small"
-              icon="el-icon-message-solid"
-              class="message-btn"
-            ></el-button>
+            <el-button type="text" size="small" icon="el-icon-message-solid" class="message-btn"></el-button>
 
             <!-- 用户menu -->
             <div>
@@ -162,27 +110,18 @@
                   <div class="user-card">
                     <div class="user-info">
                       <div class="avatar">
-                        <img
-                          :src="userAvatar"
-                          alt=""
-                          class="lazy avatar"
-                          style="
+                        <img :src="userAvatar" alt="" class="lazy avatar" style="
                             width: 48px;
                             height: 48px;
                             cursor: pointer;
                             border-radius: 50%;
-                          "
-                        />
+                          " />
                       </div>
                       <div class="user-detail">
                         <p class="name">{{ username }}</p>
                         <div class="ore">
                           <span style="font-size: 12px">矿石:2.8w</span>
-                          <img
-                            src="@/static/assets/info/unfold.jpg"
-                            alt=""
-                            class="path"
-                          />
+                          <img src="@/static/assets/info/unfold.jpg" alt="" class="path" />
                         </div>
                       </div>
                     </div>
@@ -196,11 +135,7 @@
                         <div class="progress">
                           <span>
                             308.4/500
-                            <img
-                              src="@/static/assets/info/unfold.jpg"
-                              alt=""
-                              class="progress-path"
-                            />
+                            <img src="@/static/assets/info/unfold.jpg" alt="" class="progress-path" />
                           </span>
                         </div>
                         <div class="bar">
@@ -227,78 +162,43 @@
                   <ul class="drop-down list box">
                     <li class="drop-down">
                       <div class="open-menu">
-                        <img
-                          src="@/static/assets/info/homepage.jpg"
-                          alt=""
-                          ip="image"
-                          style="width: 20px"
-                        />
+                        <img src="@/static/assets/info/homepage.jpg" alt="" ip="image" style="width: 20px" />
                         <el-link :underline="false">我的主页</el-link>
                       </div>
                     </li>
                     <li class="drop-down">
                       <div class="open-menu" style="width: 100px">
-                        <img
-                          src="@/static/assets/info/welfare.jpg"
-                          alt=""
-                          ip="image"
-                          style="width: 20px"
-                        />
+                        <img src="@/static/assets/info/welfare.jpg" alt="" ip="image" style="width: 20px" />
                         <el-link :underline="false">成长福利</el-link>
                       </div>
                     </li>
                     <li class="drop-down">
                       <div class="open-menu">
-                        <img
-                          src="@/static/assets/info/member.jpg"
-                          alt=""
-                          ip="image"
-                          style="width: 20px"
-                        />
+                        <img src="@/static/assets/info/member.jpg" alt="" ip="image" style="width: 20px" />
                         <el-link :underline="false">会员中心</el-link>
                       </div>
                     </li>
                     <li class="drop-down">
                       <div class="open-menu">
-                        <img
-                          src="@/static/assets/info/course.jpg"
-                          alt=""
-                          ip="image"
-                          style="width: 20px"
-                        />
+                        <img src="@/static/assets/info/course.jpg" alt="" ip="image" style="width: 20px" />
                         <el-link :underline="false">我的课程</el-link>
                       </div>
                     </li>
                     <li class="drop-down">
                       <div class="open-menu">
-                        <img
-                          src="@/static/assets/info/discounts.jpg"
-                          alt=""
-                          ip="image"
-                          style="width: 20px"
-                        />
+                        <img src="@/static/assets/info/discounts.jpg" alt="" ip="image" style="width: 20px" />
                         <el-link :underline="false">我的优惠</el-link>
                       </div>
                     </li>
                     <li class="drop-down">
                       <div class="open-menu">
-                        <img
-                          src="@/static/assets/info/apply.jpg"
-                          alt=""
-                          ip="image"
-                          style="width: 20px"
-                        />
+                        <img src="@/static/assets/info/apply.jpg" alt="" ip="image" style="width: 20px" />
                         <el-link :underline="false">我的报名</el-link>
                       </div>
                     </li>
                     <li class="drop-down">
                       <div class="open-menu">
-                        <img
-                          src="@/static/assets/info/footprint.jpg"
-                          alt=""
-                          ip="image"
-                          style="width: 20px"
-                        />
+                        <img src="@/static/assets/info/footprint.jpg" alt="" ip="image" style="width: 20px" />
                         <el-link :underline="false">我的足迹</el-link>
                       </div>
                     </li>
@@ -307,25 +207,16 @@
                   <div style="display: flex; margin-top: 10px">
                     <el-link :underline="false">我的设置</el-link>
                     <div class="hide"></div>
-                    <el-link :underline="false" @click="logout()"
-                      >退出登录</el-link
-                    >
+                    <el-link :underline="false" @click="logout()">退出登录</el-link>
                   </div>
                 </div>
-                <div
-                  slot="reference"
-                  class="avatar-div"
-                  style="width: 40px; height: 40px; margin-right: 10px"
-                >
-                  <img
-                    :src="userAvatar"
-                    style="
+                <div slot="reference" class="avatar-div" style="width: 40px; height: 40px; margin-right: 10px">
+                  <img :src="userAvatar" style="
                       width: 100%;
                       height: 100%;
                       cursor: pointer;
                       border-radius: 50%;
-                    "
-                  />
+                    " />
                 </div>
               </el-popover>
               <!-- 登录前 -->
@@ -355,7 +246,7 @@ export default {
       searchChange: '',
       searchColor: '#515767',
       searchMenu: '',
-      searchWidth:'',
+      searchWidth: '',
     }
   },
   computed: {
@@ -411,7 +302,7 @@ export default {
       this.throttle(this.scrollToTop, 200)
     )
   },
-  updated() {},
+  updated() { },
   destroyed() {
     window.removeEventListener('scroll', this.scroll)
   },
@@ -453,11 +344,11 @@ export default {
       this.badgeShow = true
       this.$refs.input.$refs.input.style.width = ''
       this.$refs.input.$refs.input.placeholder = '探索稀土掘金'
-      if(this.input){
+      if (this.input) {
         this.$router.push({
           name: "Search",
-          query:{search: this.input}
-          })
+          query: { search: this.input }
+        })
       }
     },
   },
@@ -470,9 +361,11 @@ export default {
   z-index: 100;
   position: fixed;
   height: 65px;
+
   .nav-container {
     margin: 0 auto;
     width: 100%;
+
     .nav-bar {
       height: 64px;
       align-items: center;
@@ -484,28 +377,34 @@ export default {
         height: 33px;
         margin-left: 20px;
       }
+
       .logo-img {
         display: none;
         width: 31px;
       }
+
       // 媒体查询要写在下面才能生效
       @media (max-width: 640px) {
         .logo-text {
           display: none;
         }
+
         .logo-img {
           display: block;
           margin-left: 20px;
         }
+
         .el-dropdown-link {
           font-size: 13px;
         }
       }
+
       // 导航栏区域
       .left,
       .right {
         display: flex;
       }
+
       // 左侧链接部分
       .left {
         justify-content: flex-start;
@@ -524,11 +423,13 @@ export default {
         .nav-link {
           border-bottom: 1px solid white;
           margin-left: 13px;
+
           .el-menu-item {
             width: 50px;
             font-size: 16px;
             padding: 0 10px;
             transition: none !important;
+
             &.is-active {
               color: #1e80ff;
               border-bottom: none;
@@ -540,6 +441,7 @@ export default {
           .nav-link-dropdown {
             display: block;
           }
+
           .nav-link {
             display: none;
           }
@@ -553,6 +455,7 @@ export default {
         justify-content: flex-end;
         align-items: center;
         margin: 0 15px;
+
         .searchMenu {
           width: 100%;
           position: absolute;
@@ -569,16 +472,19 @@ export default {
           box-shadow: 0 1px 2px 0 rgb(0 0 0 / 5%);
           border: 1px solid #ebebeb;
           background-color: #fff;
+
           .searchHead {
             border-bottom: 1px solid #ebebeb;
             padding: 0.5rem 1rem;
             display: flex;
             justify-content: space-between;
           }
+
           .clear {
             cursor: pointer;
             color: #1e80ff;
           }
+
           .searchList {
             padding: 0.5rem 1rem;
             display: flex;
@@ -601,11 +507,13 @@ export default {
         .holder {
           width: 140px;
         }
+
         @media (max-width: 720px) {
-        .input{
-          width: 125px;
+          .input {
+            width: 125px;
+          }
         }
-        }
+
         @media (max-width: 360px) {
           .input {
             display: none;
@@ -617,18 +525,22 @@ export default {
           right: 60px;
           z-index: 100;
           height: 30px;
+
           .originator-drop {
             right: -62px;
             min-width: 140px;
             z-index: 0;
+
             &:hover {
               z-index: 0;
             }
-            & > * {
+
+            &>* {
               width: 100%;
             }
           }
         }
+
         // 消息按钮
         .message-btn {
           height: 100%;
@@ -637,32 +549,40 @@ export default {
           color: #8a919f; // #515767
           z-index: 110;
         }
+
         // border: 1px solid skyblue;
       }
     }
   }
 }
+
 .nav-leave-active {
   transition: 0.2s;
 }
+
 .nav-enter-active {
   transition: 0.2s;
 }
+
 .nav-enter,
 .nav-leave-to {
   transform: translateY(-100%);
 }
+
 .badge-leave-active {
   transition: 0.4s;
 }
+
 .badge-enter-active {
   transition: 0.4s;
 }
+
 .badge-enter,
 .badge-leave-to {
   transform: translateX(20%);
   opacity: 0;
 }
+
 .landButton {
   text-align: center;
   line-height: 28px;
@@ -676,10 +596,12 @@ export default {
   cursor: pointer;
   transition: 0.4s;
 }
+
 .landButton:hover {
   background-color: #cde1f89c;
   transition: 0.4s;
 }
+
 .search {
   width: 40px;
   height: 35px;
@@ -695,11 +617,13 @@ export default {
   transition: 0.3s;
   cursor: pointer;
 }
+
 .searchChange {
   right: 181px;
   transition: 0.3s;
   background-color: rgb(217, 236, 255);
 }
+
 .line {
   position: absolute;
   top: 63px;
@@ -708,40 +632,50 @@ export default {
   background-color: #59575718;
   z-index: 9;
 }
-.vip{
+
+.vip {
   margin: 0 10px 0 10px;
   z-index: 110
 }
+
 @media (max-width: 1100px) {
   .input {
     font-size: 13px;
   }
+
   .search {
     width: 30px;
   }
 }
+
 @media (max-width: 720px) {
   .landButton {
     margin-right: 0px;
   }
+
   .vip {
     display: none;
   }
+
   .holder {
     width: 0;
   }
+
   .badge {
-     display: none;
-   }
-   .search {
+    display: none;
+  }
+
+  .search {
     right: 275px;
-   }
+  }
 }
+
 @media (max-width: 666px) {
   .search {
     display: none;
-   }
+  }
 }
+
 // @media (max-width: 1210px) {
 //   .search {
 //     display: none;
@@ -750,7 +684,7 @@ export default {
 //     display: none;
 //   }
 // }
-  
+
 .tablead {
   position: absolute;
   top: 6px;
